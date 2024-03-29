f251c07e325ba6ebeb09bdccaa1a7b1c5a6b3c0e
Romain Mosser <mosser.romain@gmail.com>
2024-03-29 11:32:05 +0100

    added error handling for signup & login pages

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -17,18 +17,24 @@ const LoginPage = () => {
 
     // Function to collect the client's input : argument input_key has to be set to one of the form's value to be pushed to the matching key of the user object
     const handleInput = (e, input_key) => {
+        // Reset of the error message when the user's change the input
+        setErrorMessage("");
+        // Copy of the user object
         let newUser = { ...user };
+        // Variable to collect the user's input
         const input_value = e.target.value;
+        // Assignement to the user copy object
         if (input_key === "email") {
             newUser.email = input_value;
         } else if (input_key === "password") {
             newUser.password = input_value;
         }
+        // Setting the user object to its new value
         setUser(newUser);
     }
 
     const navigate = useNavigate(); // Setting a navigate variable to the useNavigate function
-    const [error, setError] = useState(false); // Setting a state to show an error modal if the login fails
+    const [errorMessage, setErrorMessage] = useState(""); // Setting a state to show an error modal if the login fails
 
     // Function to send the user's info to the API that takes two arguments :
     // api_url : has to be set to the targeted API endpoint
@@ -44,47 +50,52 @@ const LoginPage = () => {
             navigate("/"); // Redirection to the home page
         } catch (error) {
             console.log(error.message);
+            if (error.message === "Request failed with status code 400") {
+                setErrorMessage("Le mot de passe et/ou l'email n'existent pas");
+            }
         }
     }
 
     return (
         <div className="page-wrapper login-page-wrapper">
             <div className="container">
-                <h1>Se connecter</h1>
                 {Cookies.get("user_token") !== undefined ?
                     // If a user token already exists
-                    <div>
-                        <p>You're already logged in</p>
-                        <button onClick={() => {
+                    <>
+                        <h1>Vous êtes déjà connecté</h1>
+                        <button className="button button-danger" onClick={() => {
                             Cookies.remove("user_token");
                             navigate("/signup");
-                        }}>Log out</button>
-                    </div>
+                        }}>Se déconnecter</button>
+                    </>
                     :
-                    <div className="card form-container">
-                        <form onSubmit={(e) => { handleSubmit(e, `${import.meta.env.VITE_API_URL}/user/login`, user) }}>
-                            <div className="input-group">
-                                <label htmlFor="email">Email</label>
-                                <input onChange={(e) => { handleInput(e, "email") }} type="email" name="email" id="email" placeholder="Adresse email" required />
-                            </div>
-                            <div className="input-group">
-                                <label htmlFor="password">Mot de passe</label>
-                                <input onChange={(e) => { handleInput(e, "password") }} type="password" name="password" id="password" placeholder="Mot de passe" required />
-                            </div>
-                            <input className="button button-fill" type="submit" />
-                            {error &&
-                                // {/* // If the error state is set to true */}
-                                <div className="form-modal">
-                                    <h3>Erreur</h3>
-                                    <p>Aucun compte pour cet email ou mot de passe</p>
+                    <>
+                        <h1>Se connecter</h1>
+                        <div className="card form-container">
+                            <form onSubmit={(e) => { handleSubmit(e, `${import.meta.env.VITE_API_URL}/user/login`, user) }}>
+                                <div className="input-group">
+                                    <label htmlFor="email">Email</label>
+                                    <input onChange={(e) => { handleInput(e, "email") }} type="email" name="email" id="email" placeholder="Adresse email" required />
                                 </div>
-                            }
-                        </form>
-                        <div className="hint">
-                            <h4>Vous n'avez pas de compte ? </h4>
-                            <Link cla to="/signup">Créez en un ici</Link>
+                                <div className="input-group">
+                                    <label htmlFor="password">Mot de passe</label>
+                                    <input onChange={(e) => { handleInput(e, "password") }} type="password" name="password" id="password" placeholder="Mot de passe" required />
+                                </div>
+                                <input className="button button-fill" type="submit" />
+                                {errorMessage &&
+                                    // {/* // If the error state is set to true */}
+                                    <div className="form-error">
+                                        <h3>Erreur</h3>
+                                        <p>{errorMessage}</p>
+                                    </div>
+                                }
+                            </form>
+                            <div className="hint">
+                                <h4>Vous n'avez pas de compte ? </h4>
+                                <Link cla to="/signup">Créez en un ici</Link>
+                            </div>
                         </div>
-                    </div>
+                    </>
                 }
             </div>
         </div>
--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -17,9 +17,16 @@ const SignUpPage = () => {
         newsletter: false
     })
 
+    // Error message state set to an empty string : will be set to a string if an error occurs
+    const [errorMessage, setErrorMessage] = useState("");
+
     // Function to collect the client's input : argument input_key has to be set to one of the form's value to be pushed to the matching key of the user object
     const handleInput = (e, input_key) => {
+        // Error reset on input change
+        setErrorMessage("");
+        // Copy of the user object
         let newUser = { ...user };
+        // Variable collecting the client's inputs
         const input_value = e.target.value;
         if (input_key === "username") {
             newUser.username = input_value;
@@ -31,8 +38,8 @@ const SignUpPage = () => {
             newUser.newsletter = !newUser.newsletter;
         }
         setUser(newUser);
-        console.log(user);
     }
+
 
     const navigate = useNavigate(); // Setting a navigate variable to the useNavigate function
 
@@ -41,6 +48,13 @@ const SignUpPage = () => {
     // object : has to be set to the object where the form's data has been pushed
     const handleSubmit = async (e, api_url, object) => {
         e.preventDefault(); // Preventing page reload on form submit
+
+        // Setting an error message if the password is less than 8 characters and stopping the function
+        if (user.password.length <= 8) {
+            setErrorMessage("Le mot de passe doit faire plus de 8 caractères");
+            return;
+        }
+
         try {
             // Sending a post request to the API via axios with the user object
             const response = await axios.post(api_url, object);
@@ -50,50 +64,64 @@ const SignUpPage = () => {
             navigate("/"); // Redirection to the home page
         } catch (error) {
             console.log(error.message);
+            // Setting the error message depending on the recived error
+            if (error.message === "Request failed with status code 409") {
+                setErrorMessage("Cet email est déjà pris");
+            } else {
+                setErrorMessage(`Une erreur est survenue : ${error.message}`);
+            }
         }
     }
 
     return (
         <div className="page-wrapper signup-page-wrapper">
             <div className="container">
-                <h1>Créer un compte</h1>
                 {Cookies.get("user_token") !== undefined ?
                     // If a user token already exists
-                    <div>
-                        <p>You're already logged in</p>
-                        <button onClick={() => {
+                    <>
+                        <h1>Vous êtes déjà connecté</h1>
+                        <button className="button button-danger" onClick={() => {
                             Cookies.remove("user_token");
                             navigate("/signup");
-                        }}>Log out</button>
-                    </div>
+                        }}>Se déconnecter</button>
+                    </>
                     :
                     // If no user token is strored
-                    <div className="card form-container">
-                        {/* Sign up API url and user object are set as arguments */}
-                        <form onSubmit={(e) => { handleSubmit(e, `${import.meta.env.VITE_API_URL}/user/signup`, user) }}>
-                            <div className="input-group">
-                                <label htmlFor="username">Nom d'utilisateur <span>(requis)</span></label>
-                                <input onChange={(e) => handleInput(e, "username")} type="text" name="username" id="username" placeholder="Nom d'utilisateur" required />
+                    <>
+                        <h1>Créer un compte</h1>
+                        <div className="card form-container">
+                            {/* Sign up API url and user object are set as arguments */}
+                            <form onSubmit={(e) => { handleSubmit(e, `${import.meta.env.VITE_API_URL}/user/signup`, user) }}>
+                                <div className="input-group">
+                                    <label htmlFor="username">Nom d'utilisateur <span>(requis)</span></label>
+                                    <input onChange={(e) => handleInput(e, "username")} type="text" name="username" id="username" placeholder="Nom d'utilisateur" required />
+                                </div>
+                                <div className="input-group">
+                                    <label htmlFor="email">Email <span>(requis)</span></label>
+                                    <input onChange={(e) => handleInput(e, "email")} type="email" name="email" id="email" placeholder="jean.dupont@gmail.com" required />
+                                </div>
+                                <div className="input-group">
+                                    <label htmlFor="password">Mot de passe <span>(requis)</span></label>
+                                    <input onChange={(e) => handleInput(e, "password")} type="password" name="password" id="password" placeholder="Ab765Thq_xv90!=mAq" autoComplete="on" required />
+                                </div>
+                                <div className="checkbox-group">
+                                    <input onChange={(e) => handleInput(e, "newsletter")} type="checkbox" name="newseletter" id="newsletter" />
+                                    <label htmlFor="newsletter">S'inscrire à la newsletter</label>
+                                </div>
+                                <input className="button button-fill" type="submit" value="S'inscrire" />
+                            </form>
+                            {errorMessage &&
+                                <div className="form-error">
+                                    <h3>Erreur</h3>
+                                    <p>{errorMessage}</p>
+                                </div>
+                            }
+                            <div className="hint">
+                                <h4>Vous n'avez déjà un compte ? </h4>
+                                <Link to="/login">Connectez vous ici</Link>
                             </div>
-                            <div className="input-group">
-                                <label htmlFor="email">Email <span>(requis)</span></label>
-                                <input onChange={(e) => handleInput(e, "email")} type="email" name="email" id="email" placeholder="jean.dupont@gmail.com" required />
-                            </div>
-                            <div className="input-group">
-                                <label htmlFor="password">Mot de passe <span>(requis)</span></label>
-                                <input onChange={(e) => handleInput(e, "password")} type="password" name="password" id="password" placeholder="Ab765Thq_xv90!=mAq" autoComplete="on" required />
-                            </div>
-                            <div className="checkbox-group">
-                                <input onChange={(e) => handleInput(e, "newsletter")} type="checkbox" name="newseletter" id="newsletter" />
-                                <label htmlFor="newsletter">S'inscrire à la newsletter</label>
-                            </div>
-                            <input className="button button-fill" type="submit" value="S'inscrire" />
-                        </form>
-                        <div className="hint">
-                            <h4>Vous n'avez déjà un compte ? </h4>
-                            <Link to="/login">Connectez vous ici</Link>
                         </div>
-                    </div>
+                    </>
                 }
             </div>
         </div>
