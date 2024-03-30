6b9338a7d29e147cae18f8737d645a81280ceae6
Romain Mosser <mosser.romain@gmail.com>
2024-03-30 15:17:11 +0100

    added animation on loader component

---
+++ src/assets/images/vectors/LoaderIcon.jsx
@@ -0,0 +1,7 @@
+const LoaderIcon = ({ fill_color }) => {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" data-name="Layer 1" viewBox="0 0 100 100" x="0px" y="0px"><circle cx="50" cy="22" r="4" /><path d="M33,33a4,4,0,1,0-5.66,0A4,4,0,0,0,33,33Z" /><circle cx="22" cy="50" r="4" /><path d="M27.37,72.63a4,4,0,1,0,0-5.66A4,4,0,0,0,27.37,72.63Z" /><circle cx="50" cy="78" r="4" /><path d="M67,72.63A4,4,0,1,0,67,67,4,4,0,0,0,67,72.63Z" /><circle cx="78" cy="50" r="4" /><path d="M72.63,33A4,4,0,1,0,67,33,4,4,0,0,0,72.63,33Z" /></svg>
+    )
+}
+
+export default LoaderIcon;
--- src/components/Loader/index.jsx
+++ src/components/Loader/index.jsx
@@ -1,10 +1,13 @@
 // CSS component import
 import "./loader.css"
 
+import LoaderIcon from "../../assets/images/vectors/LoaderIcon";
+
 const Loader = () => {
     return (
         <div className="container loader-container">
             <div>
+                <LoaderIcon />
                 <h3>Chargement en cours...</h3>
             </div>
         </div>
